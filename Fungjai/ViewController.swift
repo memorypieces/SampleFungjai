e9408fcef66243fe35d6cb6068811fa8d62e6d95
phol <symphonia.aria@hotmail.com>
2018-03-24 19:54:52 +0700

    Project Init

---
+++ Fungjai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fungjai
+//
+//  Created by BSD_MAC_Pro2 on 24/3/18.
+//  Copyright © 2018 BSD_MAC_Pro2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Fungjai' do
+  use_frameworks!
+  pod 'Alamofire'
+  pod 'AlamofireObjectMapper', :git => "https://github.com/tristanhimmelman/AlamofireObjectMapper.git" ,:branch => "swift-4"
+  pod 'AlamofireImage'
+
+  # Pods for Fungjai
+
+  target 'FungjaiTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FungjaiUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
